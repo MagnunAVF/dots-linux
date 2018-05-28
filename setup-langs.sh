3e05ddbf45aa48478c2e10ac994a20e68930a690
Magnun A V Furtado <magnunavf@gmail.com>
2018-05-27 23:34:48 -0700

    Change rbenv to RVM

--- setup-langs.sh
+++ setup-langs.sh
@@ -6,17 +6,11 @@ sudo apt-get install nodejs -y
 sudo apt-get install npm -y
 sudo npm install -g create-react-app
 
-# Rbenv - Ruby and Rails install
-cd
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
-rbenv install 2.5.0
-rbenv global 2.5.0
+# RVM
+sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+sudo apt-get update -y
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+echo "source $HOME/.rvm/scripts/rvm" >> ~/.bashrc
 gem install bundler
 gem install rails
 
