678496abb9eeb13eaa30694b6ddcfb69ea2fe2f9
Magnun A V Furtado <magnunavf@gmail.com>
2018-05-27 17:48:59 -0700

    Create setup languages and managers script

---
+++ setup-langs.sh
@@ -0,0 +1,27 @@
+# JavaScript - Node and React
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+npm i create-react-app
+
+# Rbenv - Ruby and Rails install
+sudo apt-get update -y
+sudo apt-get install zlib1g-dev build-essential libssl-dev libreadline-dev \
+                 libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev \
+                 libcurl4-openssl-dev python-software-properties libffi-dev -y
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+rbenv install 2.5.0
+rbenv global 2.5.0
+gem install bundler
+gem install rails
+
+# Python
+sudo apt-get install python3-pip -y
+sudo pip3 install virtualenv
+
